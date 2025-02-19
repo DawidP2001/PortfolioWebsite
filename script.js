5023000c742a548d37f4921b065247493d15909f
DawidP2001 <c00273530@setu.ie>
2025-02-19 00:30:09 +0000

    Fixed an issue with tabs not working and added some extra styling to projects section

--- script.js
+++ script.js
@@ -12,71 +12,43 @@ function tabChanged(button){
     let appDevDiv = document.getElementById("appDevList");
     let gamesDiv = document.getElementById("gamesList");
     let schoolLabsDiv = document.getElementById("schoolLabsList");
+    let allDivs = [webDevDiv, mlDiv, appDevDiv, gamesDiv, schoolLabsDiv];
+
+    let webDevTab = document.getElementById("webDevTab");
+    let mlTab = document.getElementById("mlTab")
+    let appDevTab = document.getElementById("appDevTab");
+    let gamesTab = document.getElementById("gamesTab");
+    let schoolLabsTab = document.getElementById("schoolLabsTab");
+    let allTabs = [webDevTab, mlTab, appDevTab, gamesTab, schoolLabsTab];
+
+    allDivs.forEach(div => {
+        div.style.display = "none";
+    });
+
+    allTabs.forEach(tab =>{
+        tab.classList.remove("selected");
+    });
+
     switch(id){
         case "webDevTab":
             webDevDiv.style.display = "flex";
-            mlDiv.style.display = "none";
-            appDevDiv.style.display = "none";
-            gamesDiv.style.display = "none";
-            schoolLabsDiv.style.display = "none";
-
-            webDevDiv.classList.add("selected");
-            mlDiv.classList.remove("selected")
-            appDevDiv.classList.remove("selected")
-            gamesDiv.classList.remove("selected")
-            schoolLabsDiv.classList.remove("selected")
+            webDevTab.classList.add("selected");
             break 
         case "mlTab":
-            webDevDiv.style.display = "none";
             mlDiv.style.display = "flex";
-            appDevDiv.style.display = "none";
-            gamesDiv.style.display = "none";
-            schoolLabsDiv.style.display = "none";
-
-            webDevDiv.classList.remove("selected");
-            mlDiv.classList.add("selected")
-            appDevDiv.classList.remove("selected")
-            gamesDiv.classList.remove("selected")
-            schoolLabsDiv.classList.remove("selected")
+            mlTab.classList.add("selected")
             break 
         case "appDevTab":
-            webDevDiv.style.display = "none";
-            mlDiv.style.display = "none";
             appDevDiv.style.display = "flex";
-            gamesDiv.style.display = "none";
-            schoolLabsDiv.style.display = "none";
-
-            webDevDiv.classList.remove("selected");
-            mlDiv.classList.remove("selected")
-            appDevDiv.classList.add("selected")
-            gamesDiv.classList.remove("selected")
-            schoolLabsDiv.classList.remove("selected")
+            appDevTab.classList.add("selected")
             break 
         case "gamesTab":
-            webDevDiv.style.display = "none";
-            mlDiv.style.display = "none";
-            appDevDiv.style.display = "none";
             gamesDiv.style.display = "flex";
-            schoolLabsDiv.style.display = "none";
-
-            webDevDiv.classList.remove("selected");
-            mlDiv.classList.remove("selected")
-            appDevDiv.classList.remove("selected")
-            gamesDiv.classList.add("selected")
-            schoolLabsDiv.classList.remove("selected")
+            gamesTab.classList.add("selected")
             break 
         case "schoolLabsTab":
-            webDevDiv.style.display = "none";
-            mlDiv.style.display = "none";
-            appDevDiv.style.display = "none";
-            gamesDiv.style.display = "none";
             schoolLabsDiv.style.display = "flex";
-
-            webDevDiv.classList.remove("selected");
-            mlDiv.classList.remove("selected")
-            appDevDiv.classList.remove("selected")
-            gamesDiv.classList.remove("selected")
-            schoolLabsDiv.classList.add("selected")
+            schoolLabsTab.classList.add("selected")
             break 
     }
 }
