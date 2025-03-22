c6cb0739c75bb4c22dc0f1db21ff0e11aa4b51c3
DawidP2001 <c00273530@setu.ie>
2025-03-22 21:11:32 +0000

    Added ltsm and gan to projects

--- script.js
+++ script.js
@@ -2,89 +2,101 @@ const sentimentAnalysisProject = {
     title: "Sentiment Analysis Web appilcation for Reddit",
     description: `In this project I created a web application in which a user can enter a certain topic or user they want analysed. The web application makes a request to reddits API receives the posts/comments analyses the sentiment of that data, and then displays it using charts, graphs and tables.`,
     technologies: ["HTML", "CSS", "Javascript", "Chart.js", "D3.js", "Python", "Flask", "PRAW", "VADER", "transformers"],
-    github: "https://github.com/DawidP2001/SentimentAnalysisProject"
+    github: "https://github.com/DawidP2001/SentimentAnalysisProject",
+    image: "images/Projects/GAN.jpg"
 };
 
+// Machine Learning Projects
 const decisonTreeProject = {
     title: "Decision Tree",
     description: "This project uses a dataset of movie reviews and uses a decision tree to predict the sentiment of the review.",
     technologies: ["Python", "Scikit-learn"],
-    github: "https://github.com/DawidP2001/MLPortfolio/blob/main/DecisionTrees_IncomePredictor.ipynb"
+    github: "https://github.com/DawidP2001/MLPortfolio/blob/main/DecisionTrees_IncomePredictor.ipynb",
+    image: "images/Projects/Decision-Tree.svg"
 };
 
 const linearRegressionProject = {
     title: "Linear Regression",
     description: "This project uses a dataset of house prices and uses linear regression to predict the price of a house.",
     technologies: ["Python", "Scikit-learn"],
-    github: "https://github.com/DawidP2001/MLPortfolio/blob/main/LinearRegressionTesting.ipynb"
+    github: "https://github.com/DawidP2001/MLPortfolio/blob/main/LinearRegressionTesting.ipynb",
+    image: "images/Projects/Linear-Regression.png"
 };
 
 const multipleLinearRegression = {
     title: "Multiple Linear Regression",
     description: "This project uses a dataset of house prices and uses multiple linear regression to predict the price of a house.",
     technologies: ["Python", "Scikit-learn"],
-    github: "https://github.com/DawidP2001/MLPortfolio/blob/main/StudentPerformancePredictor.ipynb"
+    github: "https://github.com/DawidP2001/MLPortfolio/blob/main/StudentPerformancePredictor.ipynb",
+    image: "images/Projects/MLR.jpg"
 };
 const kNearestNeighbor = {
     title: "K-Nearest Neighbor",
     description: "This project uses a dataset of movie reviews and uses a k-nearest neighbor algorithm to predict the sentiment of the review.",
     technologies: ["Python", "Scikit-learn"],
-    github: "https://github.com/DawidP2001/MLPortfolio/blob/main/k-nearest-neighbors.ipynb"
+    github: "https://github.com/DawidP2001/MLPortfolio/blob/main/k-nearest-neighbors.ipynb",
+    image: "images/Projects/K-Nearest-Neighbor.jpg"
 }
 const naiveBayesProject = {
     title: "Naive Bayes",
     description: "This project uses a dataset of movie reviews and uses a naive bayes classifier to predict the sentiment of the review.",
     technologies: ["Python", "Scikit-learn"],
-    github: "https://github.com/DawidP2001/MLPortfolio/blob/main/Naive-Bayes.ipynb"
+    github: "https://github.com/DawidP2001/MLPortfolio/blob/main/Naive-Bayes.ipynb",
+    image: "images/Projects/Naive-Bayes.jpg"
 };
 
 const supportVectorMachineProject = {
     title: "Support Vector Machine",
     description: "This project uses a dataset of movie reviews and uses a support vector machine to predict the sentiment of the review.",
     technologies: ["Python", "Scikit-learn"],
-    github: "https://github.com/DawidP2001/MLPortfolio/blob/main/Support-Vector-Machines.ipynb"
+    github: "https://github.com/DawidP2001/MLPortfolio/blob/main/Support-Vector-Machines.ipynb",
+    image: "images/Projects/SVM.png"
 };
 
 const kMeans = {
     title: "K-Means",
     description: "This project uses a dataset of movie reviews and uses a k-means clustering algorithm to group the reviews.",
     technologies: ["Python", "Scikit-learn"],
-    github: "https://github.com/DawidP2001/MLPortfolio/blob/main/K-Means.ipynb"
+    github: "https://github.com/DawidP2001/MLPortfolio/blob/main/K-Means.ipynb",
+    image: "images/Projects/K-Means.png"
 };
-
+const LTSM = {
+    title: "LTSM",
+    description: "This project uses a dataset of movie reviews and uses a k-means clustering algorithm to group the reviews.",
+    technologies: ["Python", "Scikit-learn"],
+    github: "https://github.com/DawidP2001/MLPortfolio/blob/main/K-Means.ipynb",
+    image: "images/Projects/LTSM.png"
+};
+const GAN = {
+    title: "GAN",
+    description: "This project uses a dataset of movie reviews and uses a k-means clustering algorithm to group the reviews.",
+    technologies: ["Python", "Scikit-learn"],
+    github: "https://github.com/DawidP2001/MLPortfolio/blob/main/K-Means.ipynb",
+    image: "images/Projects/GAN.jpg"
+};
+// Other Projects
 const javaBytecodeInterpreter = {
     title: "Java Bytecode Interpreter",
     description: "This project is a Java bytecode interpreter that can run Java class files.",
     technologies: ["Java"],
-    github: "https://github.com/DawidP2001/Java-Bytecode-Interpreter"
+    github: "https://github.com/DawidP2001/Java-Bytecode-Interpreter",
+    image: "images/Projects/GAN.jpg"
 };
 
 const watorProject = {
     title: "Wator Simulation",
     description: "In this application I recreated the wator simulation using GoLang and concurrency.",
     technologies: ["GoLang"],
-    github: "https://github.com/DawidP2001/WatorProject"
+    github: "https://github.com/DawidP2001/WatorProject",
+    image: "images/Projects/GAN.jpg"
 };
 
 const assemblySpaceShooter = {
     title: "Assembly Space Shooter",
     description: "In this project I created an arcade game in easy68xk assembly language, in which a player controls a spaceship and fights invading aliens in space.",
     technologies: ["Assembly"],
-    github: "https://github.com/DawidP2001/Previous-Projects/tree/main/Assembly%20Game"
-};
-
-const secureAppDevelopment = {
-    title: "Secure App Development",
-    description: "This project is a secure Android app that encrypts messages.",
-    technologies: ["Java", "Android"],
-    github: "https://github.com/DawidP2001/Secure-App-Development-Labs"
-};
-
-const concurrentDevelopment = {
-    title: "Concurrent Development",
-    description: "This project is a multi-threaded program that simulates a bank.",
-    technologies: ["Java"],
-    github: "https://github.com/DawidP2001/ConDevLabs"
+    github: "https://github.com/DawidP2001/Previous-Projects/tree/main/Assembly%20Game",
+    image: "images/Projects/GAN.jpg"
 };
 
 window.addEventListener("scroll", function () {
@@ -156,6 +168,12 @@ function projectChanged(button){
         case "kMeans":
             setProjectDescription(kMeans);
             break;
+        case "ltsm":
+            setProjectDescription(LTSM);
+            break;
+        case "gan":
+            setProjectDescription(GAN);
+            break;
         case "javaBytecode":
             setProjectDescription(javaBytecodeInterpreter);
             break;
@@ -173,10 +191,12 @@ function setProjectDescription(project){
     let description = document.getElementById("projectDescription");
     let technologies = document.getElementById("projectTechnologies");
     let github = document.getElementById("projectGithub");
+    let image = document.getElementById("projectImage");
     title.innerText = project.title;
     description.innerText = project.description;
     technologies.innerText = project.technologies.join(", ");
     github.href = project.github;
+    image.src= project.image;
 }
 
 // Create an intersection observer to track when the element is in view
