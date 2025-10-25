8ceea98b5be1fbb5e74bce6e49416111827d80d0
Dawid Pionk <dawidpionk01@gmail.com>
2025-10-25 02:54:53 +0100

    Updated the icon card

--- portfolio/src/components/Cards/IconCard.tsx
+++ portfolio/src/components/Cards/IconCard.tsx
@@ -5,8 +5,8 @@ interface ProjectCardProps {
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ className, imgSrc }) => {
     return (
-        <div className={`bg-gray-700 rounded `+ className}>
-            <img src={imgSrc} alt={"error"} className="w-full object-cover p-1" />
+        <div className={`bg-gray-800 rounded-xl `+ className}>
+            <img src={imgSrc} alt={"error"} className="w-full object-cover p-1 " />
         </div>
     );
 }
